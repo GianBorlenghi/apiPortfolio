e10f5d358766635a20d9ab53e0135049495074ae
GianBorlenghi <86668090+GianBorlenghi@users.noreply.github.com>
2022-09-07 18:32:57 -0300

    Update IUserService.java

--- src/main/java/com/apiAP/app/services/IUserService.java
+++ src/main/java/com/apiAP/app/services/IUserService.java
@@ -7,6 +7,7 @@ import com.apiAP.app.models.User;
 public interface IUserService {
 
 	public void saveUser(User user);
+	public void deleteUser(Long id);
 	public User findByUsername(String username);
 	public boolean emailExists(String mail);
 	public List<User> listUser();
