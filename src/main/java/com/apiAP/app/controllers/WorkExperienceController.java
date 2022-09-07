e322d2018bf0c6c603c9b225ddb294fdbb6bb0e8
GianBorlenghi <86668090+GianBorlenghi@users.noreply.github.com>
2022-09-07 10:52:50 -0300

    Update WorkExperienceController.java

--- src/main/java/com/apiAP/app/controllers/WorkExperienceController.java
+++ src/main/java/com/apiAP/app/controllers/WorkExperienceController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -152,7 +153,7 @@ public class WorkExperienceController {
 	}
 
 
-	@PostMapping("/admin/editExperience/{id}")
+	@PutMapping("/admin/editExperience/{id}")
 	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<?> editExperience(
 			@PathVariable (value = "id") Long idWork,
