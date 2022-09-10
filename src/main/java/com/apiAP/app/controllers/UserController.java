ed329dca97b6a8c61e3f6b3ffc101a8b3129ee68
GianBorlenghi <86668090+GianBorlenghi@users.noreply.github.com>
2022-09-10 17:27:59 -0300

    Update UserController.java

--- src/main/java/com/apiAP/app/controllers/UserController.java
+++ src/main/java/com/apiAP/app/controllers/UserController.java
@@ -1,24 +1,26 @@
 package com.apiAP.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.apiAP.app.models.User;
-import com.apiAP.app.services.IUserService;
-
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
+import com.apiAP.app.services.IImageService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
 	@Autowired
-	private IUserService userServ;
+	private IImageService imgServ;
+	
+	@DeleteMapping("/deleteImg/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String deleteImg(@PathVariable (value="id") Long idImg) {
+		imgServ.delteImg(idImg);
+		return "Deleted";
+	}
 	
 
 
